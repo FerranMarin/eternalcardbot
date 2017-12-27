3263bf49455af148107bf0c579b7189654ad0d68
Ferran Marin <ferran.marin.llobet@gmail.com>
2017-12-27 01:00:54 +0100

    Add !hg option"

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import re
 import json
 
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
 import discord
 from config import DISCORD_TOKEN
 
@@ -30,6 +33,35 @@ def logic(card):
         return 'Multiple options: {options}'.format(options=options)
 
 
+def calc_hypergeo(params):
+    N, M, n, y = params[0], params[1], params[2], params[3]
+    if M < n:
+        print(something)
+    hipergeom = stats.hypergeom(M, n, N)
+    x = np.arange(0, n+1)
+    fmp = hipergeom.pmf(x)
+    sfmp = list()
+    sfmpyn = 0
+    for index, val in enumerate(fmp):
+        if index == 0:
+            sfmp.append(val)
+        else:
+            sfmp.append(val+sfmp[index-1])
+        if index == y:
+            sfmpyn = val
+        elif index > y and index <= n:
+            sfmpyn += val
+        else:
+            pass
+    sfmp0y = sfmp[y]
+    plt.plot(x, fmp, '--')
+    plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
+    plt.title('Distribución Hipergeométrica')
+    plt.ylabel('Probabilidad')
+    plt.xlabel('Valores')
+    plt.save('plt.jpg')
+    return sfmp0y, sfmpyn
+
 client = discord.Client()
 
 
@@ -48,6 +80,26 @@ async def on_message(message):
         if card:
             await client.send_message(message.channel, card)
 
+    if message.content.startswith('!Hg'):
+        clean_msg = message.content.replace('!Hg', '')
+        params = clean_msg.split(" ")
+        if len(params) != 4:
+            text = """
+                Error, se requieren estos parámetros:
+                N: Tamaño del mazo
+                M: Número de cartas deseadas en el mazo
+                n: Cantiadd de robos/turnos
+                x: Número de copias que se quiere
+            """
+            await client.send_message(message.channel, text)
+        else:
+            a, b = calc_hypergeo(params)
+            text = """
+                sum(0 -> X) = {a}
+                sum(X -> n) = {b}
+            """.format(a=a, b=b)
+            await client.send_message(message.channel, text)
+            await client.send_file(message.channel, "plt.jpg")
 
 @client.event
 async def on_ready():
