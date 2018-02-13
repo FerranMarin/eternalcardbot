5666ca559c139c56d4a547de2d55350884ec4c65
Ferran Marin <ferran.marin.llobet@gmail.com>
2018-02-14 00:39:37 +0100

    Add roll

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
-import matplotlib
-matplotlib.use('Agg')
-
 import re
 import json
+import random
 
 from scipy import stats
 import numpy as np
@@ -22,6 +20,34 @@ def sanitize(inp):
     return output
 
 
+def dice(to_roll, to_save):
+    dice_list = []
+    ten_list = []
+    result = 0
+    for i in range(0, to_roll):
+        dice_list.append(random.randint(1, 10))
+    dice_list = sorted(dice_list, key=int)
+    for item in dice_list[-to_save:]:
+        if item == 10:
+            x = ten()
+            ten_list.append(x)
+            result += x
+        result += item
+    return dice_list, ten_list, result
+
+
+def ten():
+    cont = True
+    tmp_result = 0
+    while cont:
+        cont = False
+        tmp = random.randint(1, 10)
+        tmp_result += tmp
+        if tmp == 10:
+            cont = True
+    return tmp_result
+
+
 def logic(card):
     options = []
     for option in card_dict.keys():
@@ -35,34 +61,6 @@ def logic(card):
                 return card_dict[option]
         return 'Multiple options: {options}'.format(options=options)
 
-
-def calc_hypergeo(params):
-    N, M, n, y = int(params[0]),int(params[1]), int(params[2]), int(params[3])
-    hipergeom = stats.hypergeom(M, n, N)
-    x = np.arange(0, n+1)
-    fmp = hipergeom.pmf(x)
-    print(fmp)
-    sfmp = list()
-    sfmpyn = 0
-    for index, val in enumerate(fmp):
-        if index == 0:
-            sfmp.append(val)
-        else:
-            sfmp.append(val+sfmp[index-1])
-        if index == y:
-            sfmpyn = val
-        elif index > y and index <= n:
-            sfmpyn += val
-        else:
-            pass
-    sfmp0y = sfmp[y]
-    plt.plot(x, fmp, '--')
-    plt.vlines(x, 0, fmp, colors='b', lw=5, alpha=0.5)
-    plt.title('Distribución Hipergeométrica')
-    plt.ylabel('Probabilidad')
-    plt.xlabel('Valores')
-    plt.savefig('plt.png', bbox_inches='tight')
-    return sfmp0y, sfmpyn
 
 client = discord.Client()
 
@@ -78,32 +76,29 @@ async def on_message(message):
         if len(clean_msg) < 3:
             return
         card = logic(clean_msg)
-        # print(card)
+        print(card)
         if card:
             await client.send_message(message.channel, card)
 
-    if message.content.startswith('!Hg'):
-        clean_msg = message.content.replace('!Hg', '')
-        params = clean_msg.split(" ")
-        print(params, len(params))   
-        if len(params) != 5:
+    if message.content.startswith('!roll'):
+        clean_msg = message.content.replace('!roll', '')
+        params = clean_msg.split("g")
+        if len(params) != 2:
             text = """
-                Error, se requieren estos parámetros:
-                N: Tamaño del mazo
-                M: Número de cartas deseadas en el mazo
-                n: Cantiad de robos/turnos
-                x: Número de copias que se quiere
+                Error, el input ha de ser XgY:
+                - X es el numero de dados a tirar
+                - Y es el numero de dados a guardar
             """
             await client.send_message(message.channel, text)
         else:
-            params.pop(0)
-            a, b = calc_hypergeo(params)
+            a, b, c = dice(int(params[0]), int(params[1]))
             text = """
-                sum(0 -> X) = {a}
-                sum(X -> n) = {b}
-            """.format(a=a, b=b)
+                Todas las Tiradas: {a}
+                Resultados del 10: {b}
+                Resultado: {c}
+            """.format(a=a, b=b, c=c)
             await client.send_message(message.channel, text)
-            await client.send_file(message.channel, "plt.png")
+
 
 @client.event
 async def on_ready():
